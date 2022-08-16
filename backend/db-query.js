3b57531154f6ce36b7bcdca8997d15e86fe63a11
Abderrazzaq LAANAOUI <48630885+abderrazzaq-laanaoui@users.noreply.github.com>
2022-08-16 11:26:17 +0000

    unworking version of orbit-db

--- backend/db-query.js
+++ backend/db-query.js
@@ -1,42 +1,261 @@
-import { create } from 'ipfs'
+import Identities from 'orbit-db-identity-provider'
 import OrbitDB from 'orbit-db'
-const config = {
-  ipfs: {
-    preload: {
-      enabled: false
-    },
-    config: {
-      Addresses: {
-        Swarm: [
-          '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-          '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-          '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/'
-        ]
+import * as IPFS from 'ipfs'
+class ImageStore {
+
+
+  constructor() {
+    this.ipfs = null;
+    this.odb = null;
+    this.feed = null;
+  }
+
+  async connect(ipfs, options = {}) {
+    //set up orbitdb
+    this.ipfs = ipfs;
+    const identity =
+      options.identity || (await Identities.createIdentity({ id: "user" }));
+    console.log("identity", identity);
+    this.odb = await OrbitDB.createInstance(ipfs, {
+      offline: true,
+      id: identity,
+      directory: "./odb",
+    });
+
+    const publicAccess = true;
+    this.feed = await this.odb.open("testOrbit01", {
+      create: true, // If database doesn't exist, create it
+      overwrite: true, // Load only the local version of the database, don't load the latest from the network yet
+      localOnly: false,
+      type: "docstore", //eventlog,feed,keyvalue,docstore,counter
+      // If "Public" flag is set, allow anyone to write to the database,
+      // otherwise only the creator of the database can write
+      accessController: {
+        write: publicAccess ? ["*"] : [orbitdb.identity.id],
+      },
+    });
+    await this.loadIamges();
+    this.isOnline = true;
+  }
+
+  addImageToStore = (entry) => {
+    if (this.posts.filter((e) => { return e.hash === entry.hash; }).length === 0) {
+      const newImageObj = {
+        hash: entry.hash,
+        subject: entry.payload.value.subject || entry.payload.value.name,
+        body: entry.payload.value.body,
+        createdAt: entry.payload.value.createdAt,
+        address: entry.payload.value.address,
+      };
+      console.log("adding newImageObj to images store", newImageObj);
+      this.posts.push(newImageObj);
+    }
+  };
+
+  async loadIamges() {
+    // this.feed = await this.odb.feed(this.odb.identity.id + '/playlists')
+
+    this.feed.events.on("replicated", async (dbAddress, count, newFeed, d) => {
+      // this.feed = await newFeed.load(); (seems not to work correctly)
+      console.log("replicated - loading posts from db");
+      console.log("dbAddress", dbAddress);
+      console.log("count", count);
+      console.log("feed", newFeed);
+      newFeed.all.map(this.addImageToStore);
+      //remove remotely deleted entries from playlist store
+    });
+
+    this.feed.events.on("write", async (hash, entry, heads) => {
+      console.log("wrote something adding to Posts" + hash, entry);
+      this.addImageToStore(entry);
+    });
+
+    // When the database is ready (ie. loaded), display results
+    this.feed.events.on("ready", (dbAddress, feedReady) => {
+      console.log("database ready " + dbAddress, feedReady);
+      // this.feed = feedReady
+      if (this.feed !== undefined) this.feed.all.map(this.addImageToStore);
+      else console.log('feed is still undefined although ready')
+
+    });
+
+    this.feed.events.on("replicate.progress", async (dbAddress, hash, obj) => {
+      console.log("replicate.progress", dbAddress, hash, obj);
+      // this.feed = await this.feed.load(); doesn't seem to be useful here.
+      const entry = await this.feed.get(hash);
+      for (let i = 0; i < this.posts.length; i++) {
+        console.log(">", this.posts[i].hash, this.posts[i].address);
+        if (this.posts[i].hash === entry.hash) {
+          console.log(
+            "removed playlist from store because it was deleted on another node",
+            entry.hash
+          );
+          this.playlist = this.posts.splice(i, 1);
+        }
+      }
+    });
+    await this.feed.load();
+  }
+
+  /**
+   * Create a new feed for every post
+   */
+  async createNewPost() {
+    console.log("creating new postFeed", this.currentImage.subject)
+    const postsFeed = await this.odb.feed(this.currentImage.subject, {
+      accessController: { type: "orbitdb", write: [this.odb.identity.id] },
+    })
+
+    const p = {
+      subject: this.currentImage.subject,
+      body: this.currentImage.body,
+      createdAt: this.currentImage.createdAt ? this.currentImage.createdAt : new Date().getTime(),
+      address: postsFeed.address.toString(),
+    }
+
+    const hash = await this.feed.add(p);
+    return hash;
+  }
+
+  async removePost() {
+    const filteredData = this.posts.filter((item) => {
+      return item.hash !== this.currentImage.hash
+    });
+
+    this.posts.replace(filteredData);
+    const hash = await this.feed.remove(this.currentImage.hash);
+    return hash;
+  }
+
+  previousPost(address) {
+    if (!address) return "please provide address of current post"
+    console.log(`loading previous post of address ${address}`);
+    for (var i = 0; i < this.posts.length; i++) {
+      if (this.posts[i].address === address) {
+        const previousPost = { address: this.posts[i - 1]?.address, subject: this.posts[i - 1]?.subject }
+        return previousPost
+        break;
       }
     }
   }
+
+  nextPost(address) {
+    if (!address) return "please provide address of current post"
+    console.log(`loading next post of address ${address}`);
+    for (var i = 0; i < this.posts.length; i++) {
+      if (this.posts[i].address === address) {
+        const nextPost = { address: this.posts[i + 1]?.address, subject: this.posts[i + 1]?.subject }
+        return nextPost
+        break;
+      }
+    }
+  }
+
+  //   if (this.odb) {
+  //     console.log('loading comments of post ',nextAddress)
+  //     const imagePost = this.odb.stores[nextAddress] || (await this.odb.open(nextAddress));
+  //     await imagePost.load();
+  //     this.currentFeed = imagePost; 
+  //   }else console.log('odb not loaded')
+
+  // }
+
+  async joinImageItem(address) {
+    console.log("joinImageItem - loading address", address);
+    if (this.odb) {
+      const ourImage = this.posts.filter((item) => { return item.address === address })
+      this.currentImage = ourImage.length > 0 ? ourImage[0] : undefined
+      try {
+        const imageItem =
+          this.odb.stores[address] || (await this.odb.open(address));
+        await imageItem.load();
+        this.currentFeed = imageItem
+      } catch (ex) {
+        console.log(ex, 'comments feed could not loaded')
+      }
+    } else console.log('odb not loaded')
+  }
+
+  sendFiles(files, address) {
+    const promises = [];
+    for (let i = 0; i < files.length; i++) {
+      promises.push(this._sendFile(files[i], address));
+    }
+    return Promise.all(promises);
+  }
+
+  async _sendFile(file, address) {
+    console.log("file", file);
+    console.log("address", address);
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const f = await this.addFile(address, {
+          filename: file.name,
+          buffer: event.target.result,
+          meta: { mimeType: file.type, size: file.size },
+        });
+        resolve(f);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  async addFile(address, source) {
+    if (!source || !source.filename) {
+      throw new Error("Filename not specified");
+    }
+    const isBuffer = source.buffer && source.filename;
+    const name = source.filename.split("/").pop();
+    console.log("filename", name);
+    const size = source.meta && source.meta.size ? source.meta.size : 0;
+
+    const result = await this.ipfs.add(Buffer.from(source.buffer));
+    console.log("result of ipfs.add", result);
+    const hash = result.path;
+
+    console.log("upload hash (cid)", hash);
+
+    // Create a post
+    const data = {
+      content: hash,
+      meta: Object.assign(
+        {
+          from: this.odb.identity.id,
+          type: "file",
+          ts: new Date().getTime(),
+        },
+        { size, name },
+        source.meta || {}
+      ),
+    };
+
+    return await this.addImage(address, data);
+  }
+
+  async addImage(address, data) {
+    console.log("adding data to db on address", address);
+    const imageItem = this.odb.stores[address] || (await this.odb.open(address));
+    if (imageItem) {
+      const hash = await imageItem.add(data);
+      console.log("got hash", hash);
+      await imageItem.load();
+      this.currentFeed = imageItem;
+      console.log("blogPost feed loaded", this.currentFeed);
+      return imageItem.get(hash);
+    }
+    return;
+  }
 }
 
-export default config
+const store = global.store = new ImageStore();
+const ipfs = IPFS.create({ silent: true });
 
-  ; (async function () {
-    const ipfs = await create(config.ipfs)
-    const orbitdb = await OrbitDB.createInstance(ipfs)
-
-    // Create / Open a database
-    const db = await orbitdb.log("hello")
-    await db.load()
-
-    // Listen for updates from peers
-    db.events.on("replicated", address => {
-      console.log(db.iterator({ limit: -1 }).collect())
-    })
-
-    // Add an entry
-    const hash = await db.add("world")
-    console.log(hash)
-
-    // Query
-    const result = db.iterator({ limit: -1 }).collect()
-    console.log(JSON.stringify(result, null, 2))
-  })()
+store.connect(ipfs).then(() => {
+  console.log("store connected");
+}).catch((err) => {
+  console.log("store error", err);
+}).finally(() => {
+  console.log("store finally");
+})
+export default store
